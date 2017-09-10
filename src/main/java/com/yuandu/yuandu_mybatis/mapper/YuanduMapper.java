8083fdb742ef8c4d1a1e9a7dc8ba5adac4456664
longyuanbing <longyuanbing@lifesense.com>
2017-09-10 15:16:00 +0800

    aa

---
+++ src/main/java/com/yuandu/yuandu_mybatis/mapper/YuanduMapper.java
@@ -0,0 +1,8 @@
+package com.yuandu.yuandu_mybatis.mapper;
+
+import tk.mybatis.mapper.common.Mapper;
+import tk.mybatis.mapper.common.MySqlMapper;
+
+public interface YuanduMapper<T> extends Mapper<T>, MySqlMapper<T> {
+
+}
