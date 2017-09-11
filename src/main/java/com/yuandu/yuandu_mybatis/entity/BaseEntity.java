60c39222813f40582f1ba008a34e9d6368bc2e3e
longyuanbing <longyuanbing@lifesense.com>
2017-09-11 08:28:02 +0800

    aaa

---
+++ src/main/java/com/yuandu/yuandu_mybatis/entity/BaseEntity.java
@@ -0,0 +1,9 @@
+package com.yuandu.yuandu_mybatis.entity;
+
+import java.io.Serializable;
+
+/**
+ *
+ */
+public class BaseEntity implements Serializable {
+}
--- src/main/java/com/yuandu/yuandu_mybatis/model/BaseModel.java
+++
@@ -1,9 +0,0 @@
-package com.yuandu.yuandu_mybatis.model;
-
-import java.io.Serializable;
-
-/**
- *
- */
-public class BaseModel implements Serializable {
-}
